20ea528851be7488bae506ee89d1181733fe543e
demialko <demialko@gmail.com>
2021-04-09 10:26:27 -0300

    Primer commit
    Creación del proyecto, validación de ingreso de cantidad de equipos y los nombres de los mismos.

---
+++ A891609.Actividad02/A891609.Actividad02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A891609.Actividad02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadEquipos = 0;
+            bool ingresoCorrecto = false;
+            List<String> listaDeEquipos = new List<string> { };
+            do
+            {
+                Console.WriteLine("Ingrese la cantidad de equipos que jugarán el torneo (la misma no puede ser mayor a 10)");
+                ingresoCorrecto = int.TryParse(Console.ReadLine(), out cantidadEquipos);
+            } while (!ingresoCorrecto);
+
+            for (int i = 0; i < cantidadEquipos; i++)
+            {
+                string nombre = "";
+                do
+                {
+                    Console.WriteLine("Ingrese el nombre del equipo número " + (i + 1));
+                    nombre = Console.ReadLine();
+                } while (nombre == "");
+                listaDeEquipos.Add(nombre);
+            }
+
+            Console.WriteLine(cantidadEquipos);
+            foreach (var equipo in listaDeEquipos)
+            {
+                Console.WriteLine(equipo);
+            }
+            Console.ReadKey();
+        }
+    }
+}
